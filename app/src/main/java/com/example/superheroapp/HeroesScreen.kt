78e943aada54700396f2b1f8f44e09ca713719d3
unknown <b1021116@fun.ac.jp>
2024-03-12 01:09:28 +0900

    create heroesscreen

--- app/src/main/java/com/example/superheroapp/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroapp/HeroesScreen.kt
@@ -1,2 +1,136 @@
 package com.example.superheroapp
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.superheroapp.model.Hero
+import com.example.superheroapp.model.HeroRepository
+
+@Composable
+fun HeroesScreen() {
+    Scaffold(
+        topBar = {
+            SuperHeroAppTopBar()
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            contentPadding = innerPadding,
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            items(HeroRepository.heroes) { hero ->
+                HeroesListItem(
+                    hero = hero,
+                    modifier = Modifier
+                        .padding(horizontal = dimensionResource(id = R.dimen.padding_medium))
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HeroesListItem(
+    hero: Hero,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.elevatedCardElevation(
+            defaultElevation = dimensionResource(id = R.dimen.card_elevation)
+        ),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.padding_medium))
+                .fillMaxWidth()
+                .height(dimensionResource(id = R.dimen.card_content_height))
+        ) {
+            HeroInfo(
+                modifier = Modifier.weight(1f),
+                heroName = hero.nameRes,
+                heroDescription = hero.descriptionRes
+            )
+            Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.padding_medium)))
+            HeroImage(
+                heroImage = hero.imageRes
+            )
+        }
+    }
+}
+
+@Composable
+fun HeroInfo(
+    modifier: Modifier = Modifier,
+    @StringRes heroName: Int,
+    @StringRes heroDescription: Int
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = heroName),
+            style = MaterialTheme.typography.displaySmall
+        )
+        Text(
+            text = stringResource(id = heroDescription),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+fun HeroImage(
+    @DrawableRes heroImage: Int
+) {
+    Image(
+        modifier = Modifier
+            .size(dimensionResource(id = R.dimen.card_hero_image_size))
+            .clip(MaterialTheme.shapes.small),
+        painter = painterResource(id = heroImage),
+        contentDescription = "HeroImage",
+        contentScale = ContentScale.Crop
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SuperHeroAppTopBar(
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        }
+    )
+}
